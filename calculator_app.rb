cd2dcc8ef115382540861a099422d0d2b770ec76
Michael Smith <michaelsmith19951@yahoo.com>
2018-06-12 13:16:41 -0400

    Fixed divide by zero error & cleaned up code

--- calculator_app.rb
+++ calculator_app.rb
@@ -6,20 +6,20 @@ end
 
 
 #Subtracts any two integers that the user inputs.
-def subtract(number_three, number_four)
-number_three - number_four
+def subtract(number_one, number_two)
+number_one - number_two
 end
 
 
 #Multiplies any two integers together that the user inputs.
-def multiply(number_five, number_six)
-number_five * number_six
+def multiply(number_one, number_two)
+number_one * number_two
 end
 
 
 #Divides any two integers that the user inputs.
-def divide(number_seven, number_eight)
-number_seven / number_eight
+def divide(number_one, number_two)
+number_one / number_two
 end
 
 #Allow user input
@@ -29,79 +29,30 @@ puts "What operation do you want to do?"
 action = gets.chomp
 puts "What is your second number?"
 number_two = gets.to_i
-puts "Here is the answer to the numbers you inputted."
+if action == "/" && number_two == 0
+puts "Please enter a number other than 0."
+abort
+	
 
-#Print the results of the math to the console
-puts number_one
-puts action
-puts number_two
+else
+puts "Hello human! You asked me to complete the following operation, #{number_one} #{action} #{number_two} and the result was"
+
 
 #Conditional statement that decides whether to add or subtract
-if action == "+"
+if action == "+" 
 puts addition(number_one, number_two)
 	elsif action == "-"
 puts subtract(number_one, number_two)
 end
 
+
 #Conditional statement that decides whether to multiply or divide
 if action == "*"
 puts multiply(number_one, number_two)
-	elsif action == "/"
+	elsif action == "/" 
 puts divide(number_one, number_two)
 end
-
-
-#unused code
-
-# puts "What two integers do you want to add together?"
-
-# #Allows input from the user. It can be any integer.
-# number_one = gets.to_i
-# number_two = gets.to_i
-
-
-
-
-# puts "Here is the result of the two integers you chose to add together."
-# #Add the two numbers that were inputted together
-# puts  number_one + number_two
-
-
-# puts "What two integers do you want to subtract?"
-
-# #Allows input from the user. It can be any integer.
-# number_three = gets.to_i
-# number_four = gets.to_i
-
-# puts "Here is the result of the two integers you chose to subtract."
-# #Add the two numbers that were inputted together
-# puts  number_three - number_four
-
-
-# puts "What two integers do you want to multiply together?"
-
-# #Allows input from the user. It can be any integer.
-# number_five = gets.to_i
-# number_six = gets.to_i
-
-# puts "Here is the result of the two integers you chose to multiply together."
-# #Add the two numbers that were inputted together
-# puts  number_five * number_six
-
-
-# puts "What two integers do you want to divide together?"
-
-# #Allows input from the user. It can be any integer.
-# number_seven = gets.to_i
-# number_eight = gets.to_i
-
-# puts "Here is the result of the two integers you chose to divide together."
-# #Add the two numbers that were inputted together
-# puts  number_seven / number_eight
-
-
-
-
+end
 
 
 
